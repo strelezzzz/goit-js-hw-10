e268323e4429af82978653d17639a996150beefd
Taras Bondar <strelezzzz@gmail.com>
2022-06-04 01:00:07 +0300

    add f-message

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { fetchCountries } from './js/fetchCountries.js';
 // import countryCardsTpl from './countryCards.hbs';
 import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import Notiflix from 'notiflix';
 const DEBOUNCE_DELAY = 300;
 // ==============================================================
 countryList = document.querySelector('.country-list');
@@ -14,6 +15,8 @@ function onInput(e) {
   const input = e.target.value.trim();
   console.log(input);
   if (input === '') {
+    failureMessage();
+    moreSpecificMessage();
     return;
   }
 
@@ -23,7 +26,7 @@ function onInput(e) {
         //===============================================
         const markup = `<div class="card">
     <div class="card-flag">
-        <img src="${flags.svg}" width = "36px" alt="${name.common} flag">
+        <img src="${flags.svg}" width = "55px" alt="${name.official} flag">
     </div>
     <div class="card-body">
         <h2 class="card-name">${name.official}</h2>
@@ -41,6 +44,11 @@ function onInput(e) {
     });
 }
 
-// }
-
-// fetchCountries(name);
+function failureMessage() {
+  Notiflix.Notify.failure('Oops, there is no country with that name.');
+}
+function moreSpecificMessage() {
+  Notiflix.Notify.info(
+    'Too many matches found. Please enter a more specific name.'
+  );
+}
