21ef382343ed2d174add484fa91e3719f10b82c8
Taras Bondar <strelezzzz@gmail.com>
2022-06-04 02:22:23 +0300

    markup-f

--- src/index.js
+++ src/index.js
@@ -15,32 +15,32 @@ function onInput(e) {
   const input = e.target.value.trim();
   console.log(input);
   if (input === '') {
-    failureMessage();
-    moreSpecificMessage();
+    countryInfo.innerHTML = '';
+    countryList.innerHTML = '';
     return;
   }
 
   fetchCountries(input)
     .then(country => {
-      country.map(({ name, capital, population, languages, flags }) => {
-        //===============================================
-        const markup = `<div class="card">
-    <div class="card-flag">
-        <img src="${flags.svg}" width = "55px" alt="${name.official} flag">
-    </div>
-    <div class="card-body">
-        <h2 class="card-name">${name.official}</h2>
-        <p class="card-text">Capital:${capital}</p>
-        <p class="card-population">Population:${population}</p>
-        <p class="card-languages">Languages:${Object.values(languages)}</p>
-        </div>
-    </div>`;
-        console.log(markup);
-        countryInfo.innerHTML = markup;
-      });
+      if (country.length > 10) {
+        moreSpecificMessage();
+        countryInfo.innerHTML = '';
+        countryList.innerHTML = '';
+        return;
+      }
+      if (10 >= country.length && country.length > 1) {
+        markupCoutnryList(country);
+        return;
+      }
+
+      markupCoutnryInfo(country);
+      return;
     })
     .catch(error => {
       console.log(error);
+      failureMessage();
+      // countryInfo.innerHTML = '';
+      // countryList.innerHTML = '';
     });
 }
 
@@ -51,4 +51,35 @@ function moreSpecificMessage() {
   Notiflix.Notify.info(
     'Too many matches found. Please enter a more specific name.'
   );
+}
+
+function markupCoutnryInfo(obj) {
+  obj.map(({ name, capital, population, languages, flags }) => {
+    const markup = `<div class="card">
+    <div class="card-flag">
+        <img src="${flags.svg}" width = "55px" alt="${name.official} flag">
+    </div>
+    <div class="card-body">
+        <h2 class="card-name">${name.official}</h2>
+        <p class="card-text">Capital:${capital}</p>
+        <p class="card-population">Population:${population}</p>
+        <p class="card-languages">Languages:${Object.values(languages)}</p>
+        </div>
+    </div>`;
+    console.log(markup);
+    countryInfo.innerHTML = markup;
+  });
+}
+function markupCoutnryList(obj) {
+  const markup = obj
+    .map(
+      ({ name, flags }) =>
+        `<div class="card">
+         <img src="${flags.svg}" width = "34px" alt="${name.official} flag">
+         <span class="card-name">${name.official}</span>
+       </div>`
+    )
+    .join('');
+  console.log(markup);
+  countryInfo.innerHTML = markup;
 }
