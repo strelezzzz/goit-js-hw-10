f8e6115a826ca3df8caf8696bcf34126fe62cabf
Taras Bondar <strelezzzz@gmail.com>
2022-06-04 00:48:21 +0300

    simple markup

--- src/index.js
+++ src/index.js
@@ -1,46 +1,46 @@
 import './css/styles.css';
+import { fetchCountries } from './js/fetchCountries.js';
 // import countryCardsTpl from './countryCards.hbs';
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const DEBOUNCE_DELAY = 300;
+// ==============================================================
+countryList = document.querySelector('.country-list');
+countryInfo = document.querySelector('.country-info');
+inputField = document.querySelector('#search-box');
+inputField.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+// =================================================================
+function onInput(e) {
+  const input = e.target.value.trim();
+  console.log(input);
+  if (input === '') {
+    return;
+  }
 
-// https://restcountries.com/v3.1/name/{name}
-// function fetchCountries(name) {
-
-//FILTER RESPONSE
-// You can filter the output of your request to include only the specified fields.
-//restcountries.com/v2/{service}?fields={field},{field},{field}
-//restcountries.com/v2/all?fields=name,capital,currencies
-// =======================================
-const name = 'canada';
-fetch(
-  `https://restcountries.com/v3.1/name/${name}/?fields=name,capital,population,languages,flags`
-)
-  .then(response => {
-    return response.json();
-  })
-  .then(country => {
-    console.log(country);
-    country.map(({ name, capital, population, languages, flags }) => {
-      // // ====================
-      const markup = `<div class="card">
+  fetchCountries(input)
+    .then(country => {
+      country.map(({ name, capital, population, languages, flags }) => {
+        //===============================================
+        const markup = `<div class="card">
     <div class="card-flag">
         <img src="${flags.svg}" width = "36px" alt="${name.common} flag">
     </div>
     <div class="card-body">
-        <h2 class="card-name">${name.common}</h2>
+        <h2 class="card-name">${name.official}</h2>
         <p class="card-text">Capital:${capital}</p>
         <p class="card-population">Population:${population}</p>
         <p class="card-languages">Languages:${Object.values(languages)}</p>
         </div>
     </div>`;
-      console.log(markup);
+        console.log(markup);
+        countryInfo.innerHTML = markup;
+      });
+    })
+    .catch(error => {
+      console.log(error);
     });
+}
 
-    // ======================//markup
-    //refs.cardContainer.innerHTML = markup;
-  })
-  .catch(error => {
-    console.log(error);
-  });
 // }
 
 // fetchCountries(name);
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,7 +1,15 @@
+export { fetchCountries };
 function fetchCountries(name) {
-  fetch(
-    `https://restcountries.com/v3.1/name/${name}fields=name,capital,population,languages,flags`
+  return fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,languages,flags`
   ).then(response => {
     return response.json();
   });
 }
+// https://restcountries.com/v3.1/name/{name}
+
+//FILTER RESPONSE
+// You can filter the output of your request to include only the specified fields.
+//restcountries.com/v2/{service}?fields={field},{field},{field}
+//restcountries.com/v2/all?fields=name,capital,currencies
+// =======================================
