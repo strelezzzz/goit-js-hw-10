12c964e468ef55219ae36bc196b7d55354c1793a
strelezzzz <48587867+strelezzzz@users.noreply.github.com>
2022-06-03 16:59:02 +0000

    Deploying to gh-pages from @ strelezzzz/goit-js-hw-10@3cd13f2deb4b0de0c66407db7275a34f27d5886e 🚀

---
+++ index.e509283d.js
@@ -0,0 +1,2 @@
+fetch("https://restcountries.com/v3.1/name/brasil?fields=name,capital,population,languages").then((function(n){return n.json()})).then((function(n){console.log(n)})).catch((function(n){console.log(n)}));
+//# sourceMappingURL=index.e509283d.js.map
---
+++ index.f9f9d64e.js
@@ -0,0 +1,2 @@
+fetch("https://restcountries.com/v3.1/name/brasil?fields=name,capital,population,languages").then((e=>e.json())).then((e=>{console.log(e)})).catch((e=>{console.log(e)}));
+//# sourceMappingURL=index.f9f9d64e.js.map
