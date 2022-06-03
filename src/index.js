204b60d47bf95f48e542f5450fdadf390e17eca3
Taras Bondar <strelezzzz@gmail.com>
2022-06-03 20:40:30 +0300

    ad test markup

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const DEBOUNCE_DELAY = 300;
 //FILTER RESPONSE
 // You can filter the output of your request to include only the specified fields.
 //restcountries.com/v2/{service}?fields={field},{field},{field}
+//restcountries.com/v2/all?fields=name,capital,currencies
 
 fetch(
   'https://restcountries.com/v3.1/name/brasil?fields=name,capital,population,languages'
@@ -17,20 +18,26 @@ fetch(
   })
   .then(country => {
     console.log(country);
-    //   ====================
-    // const markup = `<div class="card">
-    // <div class="card-flag">
-    //     <img src="" alt="">
-    // </div>
-    // <div class="card-body">
-    //     <h2 class="card-name">{${name}}</h2>
-    //     <p class="card-text">Capital:{${capital}}</p>
-    //     <p class="card-population">Population:</p>
-    //     <p class="card-languages">Languages:</p>
-    //     </div>
-    // </div>`;
+    const countryValues = country.map(
+      ({ name, capital, population, languages }) => {
+        // // ====================
+        const markup = `<div class="card">
+    <div class="card-flag">
+        <img src="" alt="">
+    </div>
+    <div class="card-body">
+        <h2 class="card-name">${name}</h2>
+        <p class="card-text">Capital:${capital}</p>
+        <p class="card-population">Population:${population}</p>
+        <p class="card-languages">Languages:</p>
+        </div>
+    </div>`;
+        console.log(markup);
+      }
+    );
+
     // ======================//markup
-    // console.log(markup); //refs.cardContainer.innerHTML = markup;
+    //refs.cardContainer.innerHTML = markup;
   })
   .catch(error => {
     console.log(error);
