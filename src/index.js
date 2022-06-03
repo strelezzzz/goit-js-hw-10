6ee5afd447a14d28e8650d11a8d257ab0404a0a6
Taras Bondar <strelezzzz@gmail.com>
2022-06-03 22:56:46 +0300

    add flag

--- src/index.js
+++ src/index.js
@@ -9,32 +9,30 @@ const DEBOUNCE_DELAY = 300;
 // You can filter the output of your request to include only the specified fields.
 //restcountries.com/v2/{service}?fields={field},{field},{field}
 //restcountries.com/v2/all?fields=name,capital,currencies
-
+// =======================================
 fetch(
-  'https://restcountries.com/v3.1/name/brasil?fields=name,capital,population,languages'
+  'https://restcountries.com/v3.1/name/canada?fields=name,capital,population,languages,flags'
 )
   .then(response => {
     return response.json();
   })
   .then(country => {
     console.log(country);
-    const countryValues = country.map(
-      ({ name, capital, population, languages }) => {
-        // // ====================
-        const markup = `<div class="card">
+    country.map(({ name, capital, population, languages, flags }) => {
+      // // ====================
+      const markup = `<div class="card">
     <div class="card-flag">
-        <img src="" alt="">
+        <img src="${flags.svg}" width = "36px" alt="${name.common} flag">
     </div>
     <div class="card-body">
-        <h2 class="card-name">${name}</h2>
+        <h2 class="card-name">${name.common}</h2>
         <p class="card-text">Capital:${capital}</p>
         <p class="card-population">Population:${population}</p>
-        <p class="card-languages">Languages:</p>
+        <p class="card-languages">Languages:${Object.values(languages)}</p>
         </div>
     </div>`;
-        console.log(markup);
-      }
-    );
+      console.log(markup);
+    });
 
     // ======================//markup
     //refs.cardContainer.innerHTML = markup;
