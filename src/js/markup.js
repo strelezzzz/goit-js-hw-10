43ad26314b616c6805f8bd15be90b78feab4f840
Taras Bondar <strelezzzz@gmail.com>
2022-06-04 11:21:27 +0300

    add styles

--- src/js/markup.js
+++ src/js/markup.js
@@ -1,36 +1,39 @@
 export { markupCoutnryInfo, markupCoutnryList, markupCleaner };
-
+// ===========================
 function markupCoutnryInfo(obj) {
   obj.map(({ name, capital, population, languages, flags }) => {
-    const markup = `<div class="card">
+    const markup = `<div class="country-info">
     <div class="card-flag">
         <img src="${flags.svg}" width = "55px" alt="${name.official} flag">
+        <h2 class="card-name">${name.official}</h2>
     </div>
     <div class="card-body">
-        <h2 class="card-name">${name.official}</h2>
-        <p class="card-text">Capital:${capital}</p>
-        <p class="card-population">Population:${population}</p>
-        <p class="card-languages">Languages:${Object.values(languages)}</p>
+        <p class="card-text"><span class="values">Capital: </span>${capital}</p>
+        <p class="card-population"><span class="values">Population: </span>${population}</p>
+        <p class="card-languages"><span class="values">Languages: </span>${Object.values(
+          languages
+        )}</p>
         </div>
     </div>`;
     console.log(markup);
     countryInfo.innerHTML = markup;
   });
 }
+// =================================
 function markupCoutnryList(obj) {
   const markup = obj
     .map(
       ({ name, flags }) =>
-        `<div class="card">
-         <img src="${flags.svg}" width = "34px" alt="${name.official} flag">
-         <span class="card-name">${name.official}</span>
-       </div>`
+        `<div class="country-list">
+            <img src="${flags.svg}" width = "34px" alt="${name.official} flag">
+            <span class="card-name">${name.official}</span>
+        </div>`
     )
     .join('');
   console.log(markup);
   countryInfo.innerHTML = markup;
 }
-
+// =================================
 function markupCleaner() {
   countryInfo.innerHTML = '';
   countryList.innerHTML = '';
